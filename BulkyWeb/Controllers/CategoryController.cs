a14afaa7db8dbadc28a4021dd852afc8942beb0f
ameen226 <hosan2662000@gmail.com>
2024-03-16 17:36:27 +0200

    Add create Category UI

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -18,5 +18,10 @@ namespace BulkyWeb.Controllers
             List<Category> categories = _db.Categories.ToList();
             return View(categories);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
