b6d6072ba438c2cd086f81e4e67214977957c843
ameen226 <hosan2662000@gmail.com>
2024-03-16 22:48:54 +0200

    Add Create category action

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -23,5 +23,15 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
