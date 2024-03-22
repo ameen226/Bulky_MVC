17ed7e41edfacf2dd0c00707a8f458177fe72f15
ameen226 <hosan2662000@gmail.com>
2024-03-22 20:31:01 +0200

    Implement Repository interface

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bulky.DataAccess.IRepository
+namespace Bulky.DataAccess.Repository.IRepository
 {
     internal interface IRepository<T> where T : class
     {
