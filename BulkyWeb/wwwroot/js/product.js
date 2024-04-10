3f44517a92f9b54e9ab31d6631b3dee0a5c6a1ec
ameen226 <hosan2662000@gmail.com>
2024-04-10 18:02:33 +0200

    Fixing some edit buttons

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -17,7 +17,7 @@ function loadDataTable() {
                 data: 'id', 
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                    <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pincil-square">Edit</i></a>
+                    <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"> Edit</i></a>
                     <a onClick=Delete('/admin/product/delete?id=${data}') class="btn btn-danger mx-2"><i class="bi bi-trash-fill">Delete</i></a>
                     </div>`
                 },
