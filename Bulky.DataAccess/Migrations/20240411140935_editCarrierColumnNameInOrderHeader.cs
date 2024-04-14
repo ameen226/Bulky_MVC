6cbf860875ba46a76b948650c17db6db7383b4ec
ameen226 <hosan2662000@gmail.com>
2024-04-14 12:04:27 +0200

    Edit OrderHeader model

---
+++ Bulky.DataAccess/Migrations/20240411140935_editCarrierColumnNameInOrderHeader.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class editCarrierColumnNameInOrderHeader : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Carier",
+                table: "OrderHeaders",
+                newName: "Carrier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Carrier",
+                table: "OrderHeaders",
+                newName: "Carier");
+        }
+    }
+}
